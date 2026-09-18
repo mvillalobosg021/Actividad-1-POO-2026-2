76add2c52c3d7f5dab5a3be8840ebd11ff22c0c5
mvillalobosg021 <mvillalobosg@unal.edu.co>
2026-09-17 19:23:43 -0500

    Ejercicio Propuesto No 12

---
+++ Ejercicio_12.java
@@ -0,0 +1,25 @@
+package ejercicio_12;
+
+public class Ejercicio_12 {
+
+    
+    public static void main(String[] args) {
+       double horaslaboradas, valorhora, porcentajeretencion;
+       double salario_bruto, salario_neto, retefuente;
+       
+       horaslaboradas=48;
+       valorhora=5000;
+       porcentajeretencion=12.5/100;
+       salario_bruto = Calculos.calcular_salario_bruto(horaslaboradas, valorhora);
+       
+       retefuente= Calculos.calcular_retefuente(horaslaboradas, valorhora, porcentajeretencion);
+       salario_neto= Calculos.calcular_salario_neto(salario_bruto, retefuente);
+       
+       System.out.println("El salario bruto es: "+ salario_bruto);
+       System.out.println("El valor de la retencion es: "+ retefuente);
+       System.out.println("El salario neto es: "+ salario_neto);
+       
+        
+    }
+    
+}
